21aa34bc7f79e6bc8a3e26301145f550419d3516
Juan Souza <juan.m.souza@gmail.com>
2020-04-30 11:46:36 -0300

    first commit/folder structure

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TineEngine
+//
+//  Created by Juan Souza on 30/04/20.
+//  Copyright © 2020 Juan Souza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
